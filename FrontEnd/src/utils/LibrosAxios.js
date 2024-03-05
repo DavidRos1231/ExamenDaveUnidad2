822dd953e659a81e31f7c4a46050143488f35eff
BetjaderOD <20213tn032@utez.edu.mx>
2024-03-04 23:38:37 -0600

    Cambios

--- FrontEnd/src/utils/LibrosAxios.js
+++ FrontEnd/src/utils/LibrosAxios.js
@@ -50,8 +50,92 @@ const delateLibro = async (id) => {
 };
 
 
+const getLibrosOrderFec = async () => {
+  const url = `${baseURL}libros/get/fecha/1`;
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    console.log(response.data)
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// servicio para http://localhost:8080/api/libros/get/nombre/0
+
+const getLibrosOrderNom = async () => {
+  const url = `${baseURL}libros/get/nombre/0`;
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    console.log(response.data)
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+
+  }
+
+}
+
+
+const getLibrosOrderAut = async () => {
+  const url = `${baseURL}libros/get/autor/1`;
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    console.log(response.data)
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+
+  }
+
+
+}
+
+
+const getLibrosOrderImg = async () => {
+  const url = `${baseURL}libros/get/imagen`;
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    console.log(response.data)
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+
+  }
+}
+
+const getLibrosImagen = async () => {
+  const url = `${baseURL}libros/get/onlyImages`;
+  console.log(url)
+  try {
+    const response = await axios.get(url);
+    console.log(response.data)
+    return response.data;
+
+  } catch (error) {
+    console.error(error);
+
+  }
+}
+
 export default {
   getLibros,
   saveLibro,
-  delateLibro
+  delateLibro,
+  getLibrosOrderFec,
+  getLibrosOrderNom,
+  getLibrosOrderAut,
+  getLibrosOrderImg,
+  getLibrosImagen
+
+
+
+
 };
--- FrontEnd/src/views/Home.vue
+++ FrontEnd/src/views/Home.vue
@@ -1,30 +1,8 @@
 <template>
   <div>
-    <b-carousel id="carousel-1" v-model="slide" controls background="#ababab" img-width="1024" img-height="480"
+   <b-carousel id="carousel-1" v-model="slide" controls background="#ababab" img-width="1024" img-height="480"
       style="text-shadow: 1px 1px 2px #333;" @sliding-start="onSlideStart" @sliding-end="onSlideEnd">
-      <!-- Text slides with image -->
-      <b-carousel-slide caption="First slide" text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img class="d-block img-fluid w-100" width="1024" height="480" src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot">
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-
+      <b-carousel-slide v-for="(image, index) in images" :key="index" :img-src="image"></b-carousel-slide>
     </b-carousel>
 
 
@@ -41,9 +19,10 @@
 
               <div class=''>
                 <b-form-group>
-                  <b-button class='ml-1 mx-2' variant='primary'>Ordenar por autor</b-button>
-                  <b-button class='ml-2 mx-2' variant='primary'>Ordenar por fecha</b-button>
-                  <b-button class='ml-2 mx-2' variant='primary'>Mostrar imagen</b-button>
+                  <b-button class='ml-1 mx-2'  @click="getLibrosAutor()"  variant='primary'>Ordenar por autor</b-button>
+                  <b-button class='ml-2 mx-2' @click="getLibrosNombre()" variant='primary'>Ordenar por nombre</b-button>
+                  <b-button class='ml-2 mx-2'  @click="getLibrosFecha()" variant='primary'>Ordenar por fecha</b-button>
+                  <b-button class='ml-2 mx-2'  @click="getLibrosImagen()" variant='primary'>Mostrar imagen</b-button>
                 </b-form-group>
               </div>
             </b-form>
@@ -156,6 +135,7 @@ export default {
   data() {
     return {
             showElement: true,
+            images: [], 
 
       slide: 0,
       sliding: null,
@@ -169,10 +149,12 @@ export default {
     }
   },
 mounted() {
-    this.getLibros()
+    this.getLibros(),
+    this.getImages()
+    
   },
   methods: {
-    onDropD(event) {
+ onDropD(event) {
   console.log('Soltado para eliminar');
   const libroId = event.dataTransfer.getData("libroId");
   console.log('ID del libro soltado:', libroId);
@@ -185,12 +167,12 @@ mounted() {
       
     },
 
-    startDrag(event, libro) {
-        event.dataTransfer.setData("libroId", libro.id);
+startDrag(event, libro) {
+  event.dataTransfer.setData("libroId", libro.id);
+  console.log('Drag started');
+  console.log('Libro arrastrado:', libro); // Agregar este registro para ver la información del libro
+},
 
-      console.log('Drag started');
-      console.log(event);
-    },
     onSlideStart(slide) {
       this.sliding = true
     },
@@ -227,6 +209,66 @@ mounted() {
         alert('Error al obtener los libros')
       }
     },
+    async getLibrosFecha() {
+      try {
+        const response = await librosAxios.getLibrosOrderFec()
+        console.log(response.data)
+
+        this.libros = response.data
+
+      } catch (error) {
+        console.log(error)
+        // alert('Error al obtener los libros')
+      }
+    },
+    async getLibrosAutor() {
+      try {
+        const response = await librosAxios.getLibrosOrderAut()
+        console.log(response.data)
+
+        this.libros = response.data
+
+      } catch (error) {
+        console.log(error)
+        alert('Error al obtener los libros')
+      }
+    },
+async getLibrosImagen() {
+      try {
+        const response = await librosAxios.getLibrosOrderImg()
+        console.log(response.data)
+
+        this.libros = response.data
+
+      } catch (error) {
+        console.log(error)
+        alert('Error al obtener los libros')
+      }
+    },
+    async getLibrosNombre() {
+      try {
+        const response = await librosAxios.getLibrosOrderNom()
+        console.log(response.data)
+
+        this.libros = response.data
+
+      } catch (error) {
+        console.log(error)
+        alert('Error al obtener los libros')
+      }
+    },
+
+    async getImages () {
+      try {
+        const response = await librosAxios.getLibrosImagen()
+        console.log(response.data)
+        this.images = response.data
+      } catch (error) {
+        console.log(error)
+        alert('Error al obtener las imagenes')
+      }
+    },
+
     async onDelate (id) {
       try {
         await librosAxios.delateLibro(id)
